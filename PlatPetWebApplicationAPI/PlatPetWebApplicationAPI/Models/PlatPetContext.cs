1278f18c6cecba8613141fccf9dee2421ce532a6
Anderson Pedrassi <49460598+eaepedrassi@users.noreply.github.com>
2019-09-19 17:34:00 -0300

    API Padrão
    Gravando em memoria.
    
    URL: https://docs.microsoft.com/pt-br/aspnet/core/tutorials/first-web-api?view=aspnetcore-2.2&tabs=visual-studio

---
+++ PlatPetWebApplicationAPI/PlatPetWebApplicationAPI/Models/PlatPetContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PlatPetWebApplicationAPI.Models
+{
+    public class PlatPetContext : DbContext
+    {
+
+        public PlatPetContext(DbContextOptions<PlatPetContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PlatPetUsuario> PlatPetUsuarios { get; set; }
+
+    }
+}
